c1357bbd19e36226b5730a2042a5d5614d5a18c9
SU-Lee53 <dltmddnr05@naver.com>
2024-02-08 20:46:15 +0900

    형식 통일
    컨디션 난조로 new/delete 후반부는 내일

--- cpp_start_from_bottom/ref.cpp
+++ cpp_start_from_bottom/ref.cpp
@@ -35,7 +35,7 @@ int functionC()
 
 int main()
 {
-	//	최초의 상황
+	/* 최초의 상황 */
 	
 	//	int number = 5;
 	//	
@@ -44,7 +44,7 @@ int main()
 	//	std::cout << number << std::endl;	// 3
 
 
-	//	참조자 reference의 도입
+	/* 참조자 reference의 도입 */
 	
 	//	int a = 3;
 	//	int& anotherA = a;
@@ -61,7 +61,7 @@ int main()
 	//	std::cout << "value in pointA : " << *pointA << std::endl;
 
 
-	//	참조의 특징 1 -> 한번 참조된 후에는 다른 변수를 참조하지 못함
+	/* 참조의 특징 -> 한번 참조된 후에는 다른 변수를 참조하지 못함 */
 	
 	//	int _a = 0;
 	//	anotherA = _a;
@@ -76,7 +76,7 @@ int main()
 	//	확인결과 _a와 anotherA의 주소가 다르다
 
 
-	//	포인터 vs 참조
+	/* 포인터 vs 참조 */
 	
 	//	int a = 10;
 	//	int* p = &a;
@@ -92,24 +92,24 @@ int main()
 	//	std::cout << "adress of b : " << &b << std::endl;
 	//	std::cout << "value of p : " << *p << std::endl;
 
-	{
-		//	참조는 메모리상에 존재하지 않을 수 도 있다
+	
+	/* 참조는 메모리상에 존재하지 않을 수 도 있다*/
 
-		//	int a = 10;
-		//	int* p = &a;	// 포인터는 내부에 주소를 담아야하므로 메모리에 8바이트가 할당됨
-		//	int& ref_a = a;	// 참조는 메모리가 할당될 필요가 있을까?
+	//	int a = 10;
+	//	int* p = &a;	// 포인터는 내부에 주소를 담아야하므로 메모리에 8바이트가 할당됨
+	//	int& ref_a = a;	// 참조는 메모리가 할당될 필요가 있을까?
 
-		//	참조가 항상 메모리상에 존재하지 않는것은 아니다
+	/* 참조가 항상 메모리상에 존재하지 않는것은 아니다*/
 
-		//	int number = 5;
-		//	
-		//	std::cout << number << std::endl;
-		//	std::cout << &number << std::endl;
-		//	ChangeVal(number);
-		//	std::cout << number << std::endl;
-	}
+	//	int number = 5;
+	//	
+	//	std::cout << number << std::endl;
+	//	std::cout << &number << std::endl;
+	//	ChangeVal(number);
+	//	std::cout << number << std::endl;
+	
 
-	//	참조의 참조따위는 없다
+	/*참조의 참조따위는 없다 */
 	
 	//	int x;
 	//	int& y = x;
@@ -128,38 +128,38 @@ int main()
 	//	std::cout << "adress of y : " << &y << std::endl;
 	//	std::cout << "adress of z : " << &z << std::endl;
 
-	{
-		//	리터럴은 참조할 수 없다?
+	
+	/* 리터럴은 참조할 수 없다? */
 
-		//	int& ref = 4;
-		//	std::cout << ref << std::endl;
+	//	int& ref = 4;
+	//	std::cout << ref << std::endl;
 
-		//	리터럴도 참조할 수 있긴 하다
+	/* 리터럴도 참조할 수 있긴 하다 */
 
-		//	const int& ref = 4;
-		//	std::cout << ref << std::endl;
-	}
+	//	const int& ref = 4;
+	//	std::cout << ref << std::endl;
+	
 
-	{
-		// 참조의 배열은 불법
+	
+	/* 참조의 배열은 불법*/
 
-		//	int a, b;
-		//	int& arr[2] = { a, b };	// C2234 오류 발생
+	//	int a, b;
+	//	int& arr[2] = { a, b };	// C2234 오류 발생
 
-		//	배열의 참조는 합법
+	/* 배열의 참조는 합법 */
 
-		//	int arr[3] = { 1, 2, 3 };
-		//	int(&ref)[3] = arr;
-		//	
-		//	ref[0] = 2;
-		//	ref[1] = 3;
-		//	ref[2] = 1;
-		//	
-		//	std::cout << arr[0] << arr[1] << arr[2] << std::endl;	// 231
-	}
+	//	int arr[3] = { 1, 2, 3 };
+	//	int(&ref)[3] = arr;
+	//	
+	//	ref[0] = 2;
+	//	ref[1] = 3;
+	//	ref[2] = 1;
+	//	
+	//	std::cout << arr[0] << arr[1] << arr[2] << std::endl;	// 231
+	
 
 
-	//	지역변수의 참조를 리턴하는 함수
+	/* 지역변수의 참조를 리턴하는 함수 */
  
 	//	int b = functionA();
 	//	b = 3;
@@ -172,19 +172,19 @@ int main()
 
 
 
-	//	외부변수의 참조를 리턴하는 함수
+	/* 외부변수의 참조를 리턴하는 함수 */
 
 	//	int b = 2;
 	//	int c = functionB(b);
 	//	std::cout << &b << std::endl;
 	//	std::cout << &c << std::endl;
 
-	//	참조가 아닌 값을 리턴하는 함수를 참조자로 받기
+	/* 참조가 아닌 값을 리턴하는 함수를 참조자로 받기 */
 
 	//	int& c = functionC();
 	//	Dangling Reference!
 
-	// 위 문제의 해결법 : const 참조자
+	/* 위 문제의 해결법 : const 참조자 */
 	//	const int& c = functionC();
 	//	std::cout << c << std::endl;
 	//	std::cout << &c << std::endl;
