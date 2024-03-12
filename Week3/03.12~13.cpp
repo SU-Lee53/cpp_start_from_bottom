a0ffe15cdb574b627f9399bd460fad32a9afd7d4
SU-Lee53 <dltmddnr05@naver.com>
2024-03-12 22:14:21 +0900

    03.12
    일가야됨

---
+++ Week3/03.12~13.cpp
@@ -0,0 +1,31 @@
+
+/*
+	03.12,13 Mystring 직접 만들자
+	03.14 Mystring 모두의 코드 예제 탐구
+*/
+
+/*
+	- Mystring에 포함되어야 하는것들 :
+		1. 문자(char)로부터의 문자열 생성, C문자열(char*)로 부터의 생성
+		2. 문자열 길이를 구하는 함수
+		3. 문자열 뒤에 다른 문자열 붙이기
+		4. 문자열 내에 포함되어 있는 문자열 구하기
+		5. 문자열이 같은지 비교
+		6. 문자열 크기 비교
+*/
+
+#include <iostream>
+
+class MyString
+{
+public:
+	MyString(char* cstring);
+	MyString(const char* literal);
+
+
+private:
+
+
+
+
+};
