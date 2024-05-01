868b1efe31614a2df82c6c4e96cd80ad254ba3bb
SU-Lee53 <dltmddnr05@naver.com>
2024-05-01 12:23:36 +0900

    Update 04.30.cpp

--- Week10/04.30.cpp
+++ Week10/04.30.cpp
@@ -224,3 +224,270 @@
 //						- 만약 두번째 인자로 아무것도 전달하지 않으면 기본적으로 std::less<key>를 사용함. 이는 그냥 key 클래스의 operator<로 비교를 수행함을 의미함
 //		*/
 //	}
+
+/*  std::map  */
+//	#include <iostream>
+//	#include <map>
+//	#include <string>
+//	
+//	template<typename K, typename V>
+//	void print_map(std::map<K, V>& m)
+//	{
+//		//	for (auto itr = m.begin(); itr != m.end(); ++itr)
+//		//		std::cout << itr->first << " " << itr->second << std::endl;
+//	
+//		// 범위 기반 for 문을 이용할 수 도 있음
+//		for (const auto& kv : m)
+//			std::cout << kv.first << " " << kv.second << std::endl;
+//	}
+//	
+//	template<typename K, typename V>
+//	void search_and_print(std::map<K, V>& m, K key)
+//	{
+//		auto itr = m.find(key);
+//		if (itr != m.end())
+//			std::cout << key << " ---> " << itr->second << std::endl;
+//		else
+//			std::cout << key << "은(는) 목록에 없습니다" << std::endl;
+//	}
+//	
+//	int main()
+//	{
+//		std::map<std::string, double> pitcher_list;
+//	
+//		pitcher_list.insert(std::pair<std::string, double>("박세웅", 2.23));
+//		pitcher_list.insert(std::pair<std::string, double>("해커", 2.93));
+//		pitcher_list.insert(std::pair<std::string, double>("피어밴드", 2.95));
+//	
+//		pitcher_list.insert(std::make_pair("차우찬", 3.04));
+//		pitcher_list.insert(std::make_pair("장원준", 3.05));
+//		pitcher_list.insert(std::make_pair("핵터", 3.09));
+//	
+//		pitcher_list["니퍼트"] = 3.56;
+//		pitcher_list["박종훈"] = 3.76;
+//		pitcher_list["켈리"] = 3.90;
+//	
+//		print_map(pitcher_list);
+//	
+//		std::cout << "박세웅 방어율은? :: " << pitcher_list["박세웅"] << std::endl;
+//	
+//		// 잘못된 find - operator[] 사용시 주의
+//		//	std::cout << "류현진 방어율은? :: " << pitcher_list["류현진"] << std::endl;	// 류현진 방어율은? :: 0
+//		// 추가한적이 없는 류현진의 방어율이 출력됨 -> operator[]는 맵에 없는 키를 참조하면 자동으로 값의 디폴트 생성자를 호출하여 원소를 추가함
+//	
+//		// 올바른 find - 위 search_and_print() 함수를 참고
+//		search_and_print(pitcher_list, std::string("류현진"));	// 류현진은(는) 목록에 없습니다
+//		search_and_print(pitcher_list, std::string("박세웅"));	// 박세웅 ---> 2.23
+//	
+//		// set과 마찬가지로 중복을 무시함. 이미 같은 키가 존재한다면 나중에 오는 insert는 무시됨
+//		pitcher_list.insert(std::pair<std::string, double>("박세웅", 2.93));
+//		std::cout << "박세웅 방어율은? :: " << pitcher_list["박세웅"] << std::endl;	// 박세웅 방어율은? :: 2.23
+//	
+//		/*
+//			- output
+//			니퍼트 3.56
+//			박세웅 2.23
+//			박종훈 3.76
+//			장원준 3.05
+//			차우찬 3.04
+//			켈리 3.9
+//			피어밴드 2.95
+//			해커 2.93
+//			핵터 3.09
+//			박세웅 방어율은? :: 2.23
+//			류현진은(는) 목록에 없습니다
+//			박세웅 ---> 2.23
+//			박세웅 방어율은? :: 2.23
+//		*/
+//	
+//		/*
+//			- std::map
+//				- set과 거의 유사하지만 키와 대응되는 값까지 같이 저장함
+//				- std::map<K,V> 같은 형태로 선언하고 첫번째 인자에 키의 타입, 두번째 인자에 값의 타입이 옴
+//				- std::pair 객체로 원소들이 저장됨
+//					-> 단순히 2개의 객체를 갖는 객체임
+//					-> first, second 로 각각의 객체를 호출할 수 있고, map의 경우 first에 키, second에 값이 옴
+//					-> std::make_pair 함수를 이용해서 만들 수 도 있음
+//	
+//				- map에 원소를 추가하는 방법은 3가지임
+//					- pitcher_list.insert(std::pair<std::string, double>("박세웅", 2.23));
+//						-> 직접 pair를 만들어서 추가
+//					- pitcher_list.insert(std::make_pair("박세웅", 2.23));
+//						-> std::make_pair() 함수를 이용해 pair를 만들고 추가
+//					- pitcher_list["박세웅"] = 2.23;
+//						-> operator[]를 이용하여 추가
+//						-> 이 경우 유일하게 키가 이미 존재할경우 값이 대체됨(위에 2가지는 무시됨)
+//	
+//				- map에서 operator[]를 사용할 때 주의사항
+//					- operator[]를 이용해서 값을 찾거나 참조할 때 맵에 없는 키를 참조하면 값의 디폴트 인자를 이용해서 새로 추가해버림
+//					- 따라서 map에서 원하는 원소를 찾고자 한다면 find() 함수를 사용하는 것이 바람직함
+//						-> find() 함수의 경우 키가 맵에 존재하면 해당 pair를 가리키는 반복자를 리턴함
+//						-> 반대로 키가 맵에 없는경우 end()의 위치를 리턴함
+//	
+//				- map 역시 범위 기반 for 문이 사용 가능함
+//		*/
+//	}
+
+/*  std::multiset, std::multimap  */
+//	#include <iostream>
+//	#include <set>
+//	#include <map>
+//	#include <string>
+//	
+//	template <typename T>
+//	void print_set(const std::multiset<T>& s)
+//	{
+//		for (const auto& elem : s)
+//			std::cout << elem << std::endl;
+//	}
+//	
+//	template <typename K, typename V>
+//	void print_map(const std::multimap<K, V>& m)
+//	{
+//		for (const auto& kv : m)
+//			std::cout << kv.first << " " << kv.second << std::endl;
+//	}
+//	
+//	int main()
+//	{
+//		std::multiset<std::string> s;
+//	
+//		s.insert("a");
+//		s.insert("b");
+//		s.insert("a");
+//		s.insert("c");
+//		s.insert("d");
+//		s.insert("c");
+//	
+//		print_set(s);
+//	
+//		/*
+//			- multiset 부분 output:
+//			a
+//			a
+//			b
+//			c
+//			c
+//			d
+//		*/
+//	
+//		std::multimap<int, std::string> m;
+//		m.insert(std::make_pair(1, "hello"));
+//		m.insert(std::make_pair(1, "hi"));
+//		m.insert(std::make_pair(1, "ahihi"));
+//		m.insert(std::make_pair(2, "bye"));
+//		m.insert(std::make_pair(2, "baba"));
+//		//	m[1]; 오류: multimap은 operator[]를 사용할 수 없음
+//	
+//		print_map(m);
+//	
+//		std::cout << "-------------------------" << std::endl;
+//		std::cout << m.find(1)->second << std::endl;
+//	
+//	
+//		std::cout << "-------------------------" << std::endl;
+//		
+//		auto range = m.equal_range(1);
+//		for (auto itr = range.first; itr != range.second; ++itr)
+//		{
+//			std::cout << itr->first << " : " << itr->second << " " << std::endl;
+//		}
+//	
+//		/*
+//			- multimap 부분 output:
+//			a
+//			a
+//			b
+//			c
+//			c
+//			d
+//			1 hello
+//			1 hi
+//			1 ahihi
+//			2 bye
+//			2 baba
+//			-------------------------
+//			hello
+//			-------------------------
+//			1 : hello
+//			1 : hi
+//			1 : ahihi
+//		*/
+//	
+//		/*
+//			- std::multiset, std::multimap
+//				- 기존의 set, map 과 달리 중복을 허용하는 연관 컨테이너
+//				- multimap의 경우 기존에 사용하던 operator[]를 사용할 수 없음
+//					-> 중복을 허용하므로 중복된 키를 호출하면 어떤 값을 리턴할지 알 수 없기 때문
+//	
+//				- 또한 multimap은 중복을 허용하므로 기존의 find()의 사용에 문제가 있음
+//					-> find() 함수가 존재하긴 함. 다만 multimap의 경우 중복된 키가 있을때 어떤 값을 리턴할지에 대한 정해진 표준이 없음 
+//					-> 그러므로 위에서는 hello를 출력하였지만 다른 STL 구현체를 쓰면 뭐가 나올지 모름
+//	
+//				- 이를 위해 multiset, multimap 모두 equal_range() 라는 함수를 제공함
+//					-> equal_range()의 인자로 찾으려는 키를 전달하면 대응되는 원소들의 반복자들 중에서 시작과 끝 바로 다음을 가리키는 pair 객체를 리턴함
+//					-> pair로 리턴해주므로 first, second 에 각각 begin(), end()가 담겨있음. end()는 다른 것들과 마찬가지로 끝점 바로 뒤를 가리킴
+//						-> 그러므로 first == second 라면 대응되는 원소가 없는것임
+//		*/
+//	}
+
+/*  std::unordered_set, std::unordered_map  */
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+template <typename K>
+void print_unordered_set(const std::unordered_set<K>& m)
+{
+	for (const auto& elem : m)
+		std::cout << elem << std::endl;
+}
+
+int main()
+{
+	std::unordered_set<std::string> s;
+
+	s.insert("hi");
+	s.insert("my");
+	s.insert("name");
+	s.insert("is");
+	s.insert("psi");
+	s.insert("welcome");
+	s.insert("to");
+	s.insert("c++");
+	
+	print_unordered_set(s);
+
+	/*
+		- output
+		hi
+		welcome
+		my
+		name
+		is
+		psi
+		to
+		c++
+
+		++ vscode에서 g++을 이용하여 같은 코드를 실행한 결과
+		to
+		psi
+		is
+		welcome
+		name
+		my
+		c++
+		hi
+
+		-> STL은 같은 MSVC STL을 사용
+		-> 정렬되지 않는 컨테이너이므로 컴파일러만 바뀌어도 출력 순서가 달라짐
+	*/
+
+	/*
+		- std::unordered_set, std::unordered_map
+			- 기존의 set, map 과 달리 insert/erase/find가 모두 O(1)로 수행됨
+				-> Red-Black tree 를 사용하지 않고 해시 함수를 사용하기 때문
+			- 해시 함수
+	
+	*/
+}
