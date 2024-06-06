8d16924cf09ff3f1a1df732b7954e58e61d2c5d3
SU-Lee53 <dltmddnr05@naver.com>
2024-06-07 00:54:10 +0900

    0606

--- Week15/06.05.cpp
+++ Week15/06.05.cpp
@@ -226,31 +226,198 @@
 //	}
 
 /*  mem_fn : 멤버 함수들을 함수 객체로  */
-#include <algorithm>
+//	#include <algorithm>
+//	#include <functional>
+//	#include <iostream>
+//	#include <vector>
+//	
+//	using std::vector;
+//	
+//	int main()
+//	{
+//		vector<int> a(1);
+//		vector<int> b(2);
+//		vector<int> c(3);
+//		vector<int> d(4);
+//	
+//		vector<vector<int>> container;
+//		container.push_back(b);
+//		container.push_back(d);
+//		container.push_back(a);
+//		container.push_back(c);
+//	
+//		std::function<size_t(const vector<int>&)> sz_func = &vector<int>::size;
+//	
+//		vector<int> size_vec(4);
+//		// std::transform(container.begin(), container.end(), size_vec.begin(), &vector<int>::size); // C2064 항은 1개의 인수를 받아들이는 함수로 계산되지 않습니다.
+//		
+//		// std::transform(container.begin(), container.end(), size_vec.begin(), sz_func); // 혹은 
+//		std::transform(container.begin(), container.end(), size_vec.begin(), std::mem_fn(&vector<int>::size));
+//	
+//		// std::mem_fn 없이 람다함수를 이용해도 동일함
+//		// std::transform(container.begin(), container.end(), size_vec.begin(), [](const auto& v) -> size_t { return v.size(); });
+//	
+//	
+//		for (const auto& e : size_vec)
+//			std::cout << "벡터 크기 :: " << e << std::endl;
+//	
+//		/*
+//			- std::mem_fn
+//				- std::mem_fn : 멤버 함수를 함수 객체로 만들어 리턴해주는 함수
+//				- 위 코드 설명
+//					- 1 ~ 4 까지의 크기를 가진 vector a, b, c, d 를 만들고 각 벡터의 크기를 보관하는 size_vec 을 std::transform을 이용해 채우려는 코드
+//					- std::transform 의 서술자 부분에 &vector<int>::size 함수를 직접 넘겨주면 컴파일 오류 발생
+//						-> 이유를 알려면 <algorithm> 안에 있는 std::transform 함수의 모양을 봐야함
+//	
+//							_EXPORT_STD template <class _InIt, class _OutIt, class _Fn>
+//							_CONSTEXPR20 _OutIt transform(const _InIt _First, const _InIt _Last, _OutIt _Dest, _Fn _Func) {
+//								// transform [_First, _Last) with _Func
+//								_STD _Adl_verify_range(_First, _Last);
+//								auto _UFirst      = _STD _Get_unwrapped(_First);
+//								const auto _ULast = _STD _Get_unwrapped(_Last);
+//								auto _UDest       = _STD _Get_unwrapped_n(_Dest, _STD _Idl_distance<_InIt>(_UFirst, _ULast));
+//								for (; _UFirst != _ULast; ++_UFirst, (void) ++_UDest) {
+//									*_UDest = _Func(*_UFirst);
+//								}
+//							
+//								_STD _Seek_wrapped(_Dest, _UDest);
+//								return _Dest;
+//							}
+//	
+//						-> 위에서 서술자 함수를 실행시키는 코드는 *_UDest = _Func(*_UFirst);
+//						-> 이 부분에 &vector<int>::size 가 바로 들어가면 아래처럼 시도됨
+//	
+//							&vector<int>::size(*_UFirst);
+//	
+//						-> 멤버 함수는 (*_UFirst).(&vector<int>::size) 로 호출하는것이 맞으므로 위는 실행시킬 수 없음
+//	
+//					- 따라서 정상적인 std::transform 수행을 위해서는 &vector<int>::size 를 std::function 으로 만들어 주어야함
+//						-> 만든 결과가 위의 sz_func 이고 아래처럼 만들어줌
+//	
+//							std::function<size_t(const vector<int>&)> sz_func = &vector<int>::size
+//	
+//					- 위처럼 매번 만드는 것이 쉽지 않으므로 std::mem_fn 을 이용해서 해결함
+//						
+//						std::mem_fn(&vector<int>::size)
+//	
+//						로 함수 객체를 직접 만들어 std::transform에 넣으면 정상 작동됨
+//	
+//	
+//				- 생각보다 std::mem_fn은 자주 쓰이지 않는데 람다함수가 있기 때문임
+//					-> std::mem_fn 이 더 코드가 깔끔하긴 하지만 <functional> 헤더를 삽입해야함
+//					-> 반면 람다함수는 코드는 조금 지저분해져도 별도 헤더 없이 바로 사용 가능함
+//		
+//		
+//		*/
+//	
+//	}
+
+/*  std::bind  */
 #include <functional>
 #include <iostream>
-#include <vector>
 
-using std::vector;
+void add(int x, int y)
+{
+	std::cout << x << " + " << y << " = " << x + y << std::endl;
+}
+
+void subtract(int x, int y)
+{
+	std::cout << x << " - " << y << " = " << x - y << std::endl;
+}
+
+struct S
+{
+	int data;
+	S(int data) : data(data) { std::cout << "일반 생성자 호출" << std::endl; }	
+	S(const S& s) : data(s.data) { std::cout << "복사 생성자 호출" << std::endl; }
+	S(S&& s) : data(s.data) { std::cout << "이동 생성자 호출" << std::endl; }
+};
+
+void foo(S& s1, const S& s2) { s1.data = s2.data + 3; }
 
 int main()
 {
-	vector<int> a(1);
-	vector<int> b(1);
-	vector<int> c(1);
-	vector<int> d(1);
+	auto add_with_2 = std::bind(add, 2, std::placeholders::_1);
+	add_with_2(3);
 
-	vector<vector<int>> container;
-	container.push_back(b);
-	container.push_back(d);
-	container.push_back(a);
-	container.push_back(c);
+	// 두번째 인자는 무시됨
+	add_with_2(3, 4);
 
-	vector<int> size_vec(4);
-	std::transform(container.begin(), container.end(), size_vec.begin(), &vector<int>::size);
-	for (const auto& e : size_vec)
-		std::cout << "벡터 크기 :: " << e << std::endl;
+	auto subtract_with_2 = std::bind(subtract, std::placeholders::_1, 2);
+	auto negate = std::bind(subtract, std::placeholders::_2, std::placeholders::_1);
+
+	subtract_with_2(3);	// 3 - 2;
+	negate(4, 2);	// 2 - 4;
+
+	/*
+		- 여기까지 output :
+			2 + 3 = 5
+			2 + 3 = 5
+			3 - 2 = 1
+			2 - 4 = -2
+	*/
+
+
+	S s1(1), s2(2);
+	std::cout << "Before::s1.data :  " << s1.data << std::endl;
+
+	std::cout << "\n복사본을 전달하는 func1 --------" << std::endl;
+
+	// s1 이 그대로 전달된 것이 아니라 s1 의 복사본이 전달됨
+	auto func = std::bind(foo, s1, std::placeholders::_1);
+	func(s2);
+
+	std::cout << "After func::s1.data : " << s1.data << std::endl;
+
+	std::cout << "\n복사본 전달을 막는 func2 --------" << std::endl;
+	
+	// s1 의 복사본 전달을 막으려면 std::ref 로 참조를 넘겨주면 됨
+	auto func2 = std::bind(foo, std::ref(s1), std::placeholders::_1);
+	func2(s2);
+
+	std::cout << "After func2::s1.data : " << s1.data << std::endl;
+
+	/*
+		- output
+			일반 생성자 호출
+			일반 생성자 호출
+			Before::s1.data : 1
+			
+			복사본을 전달하는 func1 --------
+			복사 생성자 호출
+			After func::s1.data : 1
+			
+			복사본 전달을 막는 func2 --------
+			After func2::s1.data : 5
+	*/
+
+	
+
+	/*
+		- std::bind
+			- 사전적 의미 : 묶다
+			- std::bind : 말 그대로 원래 함수에 특정 인자를 묶은 함수 객체를 생성해서 리턴함
+			- bind 시킨 자리에도 인자가 전달이 가능하나 무시됨
+
+			- std::placeholders : bind 가 리턴한 새로운 함수 객체의 인자
+				- MSVC 기준 std::placeholders::_20 까지 있음
+				- std::placeholders 의 순서대로 인자가 들어감
+					-> 위의 negate 의 경우
+
+						auto negate = std::bind(subtract, std::placeholders::_2, std::placeholders::_1);
+
+					-> 이런식으로 반대로 넣어주면 negate(x, y) 수행을 y - x 로 수행함 (원래는 x - y)
+
+				- 만약 placeholder 가 부족하다면 스스로 코드를 잘못짜지는 않았는지 반성이 필요한 경우가 대부분일듯
+
+			- std::bind 사용시 주의사항 : 레퍼런스를 인자로 받는 경우
+				- 원래 레퍼런스를 인자로 받는 자리에 std::bind 를 이용해 레퍼런스가 아닌 인자를 전달하면 인자가 복사되어 전달됨
+				- 이를 막으려면 std::ref() 를 이용해 명시적으로 레퍼런스를 전달해야 함
+				- const 레퍼런스의 경우 std::cref()를 이용하면 됨
+
+				- std::ref() : 전달받은 인자를 복사 가능한 레퍼런스로 변환함
+	
+	*/
 
 }
-
-
