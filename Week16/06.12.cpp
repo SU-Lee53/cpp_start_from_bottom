464011cd9aacefd66cc75cf746b12e087ba65a6b
SU-Lee53 <dltmddnr05@naver.com>
2024-06-14 00:11:30 +0900

    0613

--- Week16/06.12.cpp
+++ Week16/06.12.cpp
@@ -199,61 +199,184 @@
 //	}
 
 /*  decltype 의 사용  */
-#include <iostream>
-#include <typeinfo>
+//	#include <iostream>
+//	#include <typeinfo>
+//	
+//	template <typename T, typename U>
+//	void add1(T t, U u, decltype(t + u)* result)
+//	{
+//		*result = t + u;
+//	}
+//	
+//	template <typename T, typename U>
+//	//	decltype(t + u) add2(T t, U u) -> decltype 안의 t, u 의 정의가 나중에 나오므로 불가능
+//	auto add2(T t, U u) -> decltype(t + u)	// C++ 14 에서 추가된 문법
+//	{
+//		return t + u;
+//	}
+//	
+//	template <typename T, typename U>
+//	decltype(auto) add3(T t, U u)			// C++ 14 에서 추가된 문법으로 위 add2를 이렇게 써도 됨
+//	{
+//		return t + u;
+//	}
+//	
+//	int main()
+//	{
+//		int a = 4;
+//		auto b = a;			// auto -> int
+//		decltype(a) c = a;	// decltype(a) -> int
+//	
+//	
+//		const int i = 4;
+//		auto j = i;			// auto -> int
+//		decltype(i) k = i;	// decltype(i) -> const int
+//	
+//		int arr[10];
+//		auto arr2 = arr;	// auto -> int * __ptr64
+//		decltype(arr) arr3;	// decltype(arr) -> int[10]
+//	
+//		int n = 10;
+//		double d = 10.0;
+//		decltype(n + d) res = 0;	// int + double 의 타입은 double
+//		decltype(n + d)* res_p = &res;
+//	
+//		add1(n, d, res_p);
+//		std::cout << typeid(res).name() << " : " << res << std::endl;	// double : 20
+//		std::cout << typeid(add2(n, d)).name() << " : " << add2(n, d) << std::endl;	// double : 20
+//		std::cout << typeid(add3(n, d)).name() << " : " << add3(n, d) << std::endl;	// double : 20
+//	
+//	
+//		/*
+//			- decltype 의 사용
+//				- auto 로 하면 안됨?
+//					- 안됨. auto 는 정확한 타입을 표현하지 못함. 예를들어
+//						
+//							const int i = 0;
+//							auto j = i;
+//	
+//						-> 위 경우 auto 는 const int 가 아닌 int 로 변환됨
+//					
+//							int arr[10];
+//							auto arr2 = arr;
+//	
+//						-> 위 경우는 int [10] 이 아닌 int* 로 변환됨
+//						-> 그 밖에 auto 가 정확하지 않은 경우는 https://en.cppreference.com/w/cpp/language/template_argument_deduction#Other_contexts 의 auto type deduction 을 참고 
+//	
+//				- decltype 은 "타입 그대로"를 정확하게 전달 가능함
+//				- 템플릿 함수에서 템플릿 인자에 대해서도 사용 가능함
+//	
+//						template <typename T, typename U>
+//						void add1(T t, U u, decltype(t + u)* result)
+//	
+//				- 함수 정의시 리턴 타입을 decltype 로 지정하고 싶다면 C++ 14 에 새로 생긴 함수 정의법을 사용해야함
+//	
+//						template <typename T, typename U>
+//						decltype(t + u) add2(T t, U u) -> X 컴파일 오류
+//	
+//						template <typename T, typename U>
+//						auto add2(T t, U u) -> decltype(t + u)	-> O 사용 가능
+//		
+//		*/
+//	
+//	}
 
-template <typename T, typename U>
-void add1(T t, U u, decltype(t + u)* result)
-{
-	*result = t + u;
-}
-
-template <typename T, typename U>
-//	decltype(t + u) add2(T t, U u) -> decltype 안의 t, u 의 정의가 나중에 나오므로 불가능
-auto add2(T t, U u) -> decltype(t + u)	// C++ 14 에서 추가된 문법
-{
-	return t + u;
-}
-
-template <typename T, typename U>
-decltype(auto) add3(T t, U u)			// C++ 14 에서 추가된 문법으로 위 add2를 이렇게 써도 됨
-{
-	return t + u;
-}
-
-
-int main()
-{
-	int a = 4;
-	auto b = a;			// auto -> int
-	decltype(a) c = a;	// decltype(a) -> int
-
-
-	const int i = 4;
-	auto j = i;			// auto -> int
-	decltype(i) k = i;	// decltype(i) -> const int
-
-	int arr[10];
-	auto arr2 = arr;	// auto -> int * __ptr64
-	decltype(arr) arr3;	// decltype(arr) -> int[10]
-
-	int n = 10;
-	double d = 10.0;
-	decltype(n + d) res = 0;	// int + double 의 타입은 double
-	decltype(n + d)* res_p = &res;
-
-	add1(n, d, res_p);
-	std::cout << typeid(res).name() << " : " << res << std::endl;	// double : 20
-	std::cout << typeid(add2(n, d)).name() << " : " << add2(n, d) << std::endl;	// double : 20
-	std::cout << typeid(add3(n, d)).name() << " : " << add3(n, d) << std::endl;	// double : 20
-
-
-	/*
-		- decltype 의 사용
-
-	
-	
-	
-	*/
-
-}
+/*  std::declval  */
+//	#include <iostream>
+//	#include <utility>
+//	#include <typeinfo>
+//	
+//	template <typename T>
+//	decltype(T().f()) call_f_and_return(T& t)
+//	{
+//		return t.f();
+//	}
+//	
+//	template <typename T>
+//	decltype(std::declval<T>().f()) declval_f_and_return(T& t)
+//	{
+//		return t.f();
+//	}
+//	
+//	// C++ 14 부터 추가된 auto 리턴
+//	template <typename T>
+//	auto auto_f_and_return(T& t)
+//	{
+//		return t.f();
+//	}
+//	
+//	struct A
+//	{
+//		int f() { return 0; }
+//	};
+//	
+//	struct B
+//	{
+//		B(int x) {}
+//		int f() { return 0; }
+//	};
+//	
+//	int main()
+//	{
+//		decltype(A().f()) ret;	// int
+//		// decltype(B().f()) ret2;	// 컴파일 불가
+//		decltype(B(1).f()) ret2;	// 가능은 함
+//	
+//		A a;
+//		B b(1);
+//	
+//		call_f_and_return(a);
+//		// call_f_and_return(b);	// 컴파일 불가
+//		declval_f_and_return(b);	// 가능
+//	
+//		auto_f_and_return(a);
+//		auto_f_and_return(b);
+//	
+//		// declval 함수는 런타임에서 사용할 수 없음
+//		// B b2 = std::declval<B>);	// E0415 : "<unknown-type>" 에서 "B"(으)로 변환하기 위한 적절한 생성자가 없습니다.
+//	
+//		/*
+//			- std::declval<T>
+//				- 위 코드에서
+//					- decltype(A().f()) ret;	
+//						- decltype 은 컴파일시에 타입으로 변환되고 런타임에는 실행되지 않음
+//						- 그러나 문법상 틀린 식은 전달할 수 없음
+//	
+//					- decltype(B().f()) ret2; 는 컴파일이 안됨
+//						- B() 생성자가 없으므로 불가능함
+//						- 굳이 B() 생성자가 필요 없는데 이걸 위해 만드는것은 비효율적
+//							- 실제 객체를 생성하지도 않고 타입 참조만 하기 때문
+//	
+//					- decltype(B(1).f()) ret2;
+//						- 가능하긴 함
+//						- 그러나 템플릿 코드 등에 들어갔을 때 문제를 일으킴
+//							-> 아래같은 함수가 있을 때
+//								template <typename T>
+//								decltype(T().f()) call_f_and_return(T& t)
+//								-> B() 생성자가 없으므로 문제가 컴파일이 불가능해짐
+//								-> 이를 해결하는 것이 std::declval
+//	
+//				- std::declval : <utility> 에 정의된 함수로 직접적인 생성자 호출 없이 객체의 멤버에 접근할 수 있도록 함
+//	
+//						template <typename T>
+//						decltype(std::declval<T>().f()) declval_f_and_return(T& t)
+//	
+//					-> 이전의 코드를 위처럼 std::declval 을 이용하면 B() 생성자가 없는 B 도 문제 없이 사용할 수 있음
+//	
+//				- std::declval 은 타입 연산에서만 사용하고 실제 런타임에는 사용할 수 없음
+//	
+//						_EXPORT_STD template <class _Ty>
+//						add_rvalue_reference_t<_Ty> declval() noexcept {
+//							static_assert(_Always_false<_Ty>, "Calling declval is ill-formed, see N4950 [declval]/2.");
+//						}
+//					
+//					- 위는 declval 함수의 원형으로 타입연산이 아닌 런타임에 실행시 static_assert 로 프로그램을 멈춰버림
+//		
+//	
+//				- C++ 14 에서 리턴타입으로 auto 로 지정하여 컴파일러가 리턴타입을 유추하는 기능이 생김
+//					- 위처럼 declval 을 이용한 리턴 타입 정의 같은 일들은 사용할 일이 줄어듬
+//					- 그러나 declval 은 <type_traits> 와 함께 TMP 에 많이 사용됨
+//		
+//		
+//		*/
+//	}
